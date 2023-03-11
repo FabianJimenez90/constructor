2de4490af81ba58e7fcbb584a7b9df538aae003a
David Jiménez <92261450+FabianJimenez90@users.noreply.github.com>
2023-03-10 19:55:40 -0500

    ejericicio de constructor

---
+++ main/main.js
@@ -0,0 +1,23 @@
+class operacion {
+    suma = (a,b) => {
+        return true;
+    };
+    resta = (a,b) => {     
+        return true;
+    };
+    multiplicacion = (a,b) => { 
+        return true;
+    };
+    division = (a,b) => {
+        return true;
+};
+constructor() {
+    this.suma = (50 + 2);
+    this.resta = (50 - 2);
+    this.multiplicacion = (50 * 2);
+    this.division = (50 / 2);
+}
+};
+const resultado = new operacion( );
+console.log(resultado);
+
